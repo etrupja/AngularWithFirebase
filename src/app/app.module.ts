15590d9ce2b2ee4f19f7e0c228ace43b71ba531e
Ervis Trupja <ervistrupja@outlook.com>
2017-07-19 13:26:51 +0000

    Authentication Implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Provider } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {RouterModule, Routes} from '@angular/router';
 import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { environment } from '../environments/environment';
 
@@ -21,7 +23,8 @@ import {FirebaseService} from './services/firebase.service';
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'listings', component: ListingsComponent},
-  {path: 'add-listings', component: AddListingComponent},
+  {path: 'listing/:id', component: ListingComponent},
+  {path: 'add-listings', component: AddListingComponent}
 ];
 
 
@@ -40,9 +43,10 @@ const appRoutes: Routes = [
     FormsModule,
     HttpModule,
     AngularFireModule.initializeApp(environment.firebase, 'my-listings-application'),
+    AngularFireDatabaseModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [FirebaseService, AngularFireDatabase],
+  providers: [FirebaseService, AngularFireDatabase, AngularFireAuth],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { AfterViewChecked, Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  user: Observable<firebase.User>;
+  authenticated: boolean = false;
+
+  constructor(public af: AngularFireAuth) {
+    this.af.authState.subscribe(
+      (auth) => {
+        if (auth != null) {
+          this.user = af.authState;
+          this.authenticated = true;
+        }
+      });
+  }
 
   ngOnInit() {
   }
 
+  login() {
+    this.authenticated = true;
+    this.af.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+   logout() {
+     this.authenticated = false;
+    this.af.auth.signOut();
+  }
 }
